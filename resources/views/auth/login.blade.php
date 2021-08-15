f93acfbda6e8bad3b9951b8fdcbbf063072bea39
iftikhar1986 <iftikhar.uddin.fazal@gmail.com>
2021-08-15 23:57:23 +0300

    User register From 1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,26 +51,30 @@
                     <div class="col-md-6 col-sm-6 create-new-account">
                         <h4 class="checkout-subtitle">Create a new account</h4>
                         <p class="text title-tag-line">Create your new account.</p>
-                        <form class="register-form outer-top-xs" role="form">
-                            <div class="form-group">
-                                <label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail2" >
-                            </div>
+
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
                             <div class="form-group">
                                 <label class="info-title" for="exampleInputEmail1">Name <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+                                <input  class="form-control unicase-form-control text-input" id="name"  type="text" name="name" >
                             </div>
                             <div class="form-group">
+                                <label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
+                                <input  class="form-control unicase-form-control text-input" id="email"  type="email" name="email" >
+                            </div>
+
+                            <div class="form-group">
                                 <label class="info-title" for="exampleInputEmail1">Phone Number <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+                                <input  class="form-control unicase-form-control text-input" id="exampleInputEmail1" id="phone"  type="text" name="phone" >
                             </div>
                             <div class="form-group">
                                 <label class="info-title" for="exampleInputEmail1">Password <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+                                <input  class="form-control unicase-form-control text-input" id="password"  type="password" name="password" >
                             </div>
                             <div class="form-group">
                                 <label class="info-title" for="exampleInputEmail1">Confirm Password <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+                                <input  class="form-control unicase-form-control text-input" id="password_confirmation"  type="password" name="password_confirmation"  >
                             </div>
                             <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Sign Up</button>
                         </form>
