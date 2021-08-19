73be6594711357a08c5ae405f76c1ca540143dc1
iftikhar1986 <iftikhar.uddin.fazal@gmail.com>
2021-08-20 01:02:19 +0300

    User Profile Design 1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,43 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+@extends('frontend.main_master')
 
+@section('content')
 
-         Hi...   {{ Auth::user()->name }}
-        </h2>
-    </x-slot>
+    <div class="body-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <br>
+                        <img class="card-img-top"  style="border-radius: 50%" src="{{ (!empty($editData->profile_photo_path))?
+                                                                              url('upload/admin_images/'.$editData->profile_photo_path):
+                                                                              url('upload/no_image.png') }}" style="width: 90%; height: 90%;">
+                    <br><br>
+                    <ul class="list-group list-group-flush">
+                        <a href="" class="btn btn-primary btn-sm btn-block">Home</a>
+                        <a href="" class="btn btn-primary btn-sm btn-block">Profile Update</a>
+                        <a href="" class="btn btn-primary btn-sm btn-block">Change Password</a>
+                        <a href="" class="btn btn-danger btn-sm btn-block">Log Out</a>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    </ul>
+                </div>   {{--  end col-md-2 --}}
 
-                This is Home Page
+                <div class="col-md-2">
 
+                </div>   {{--  end col-md-2 --}}
+
+                <div class="col-md-6">
+
+                    <div class="card">
+                        <h3 class="text-center"> <span class="text-danger">Hi...</span><strong>{{ Auth::user()->name }}</strong> Wellcome to Iftikhar Shop</h3>
+                    </div>
+
+                </div>   {{--  end col-md-6 --}}
+            </div>  {{--  end row --}}
         </div>
+
     </div>
-</x-app-layout>
+
+
+
+@endsection
+
+
